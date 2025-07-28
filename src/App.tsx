0a8bff237ed14bec173ea92eda35e6c5b1fdbdff
Nitin <nitin.dunday@un.org>
2025-07-28 17:23:02 -0400

    version_1.1:deployment

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from '@/components/ui/toaster';
 import { Toaster as Sonner } from '@/components/ui/sonner';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import ThemeToggle from './components/ThemeToggle';
 import Index from './pages/Index';
 import NotFound from './pages/NotFound';
@@ -30,13 +30,13 @@ const App = () => {
             <ThemeToggle />
           </div>
           <main>
-            <BrowserRouter>
+            <HashRouter>
               <Routes>
                 <Route path="/" element={<Index />} />
                 <Route path="/home" element={<Homescreen />} />
                 <Route path="*" element={<NotFound />} />
               </Routes>
-            </BrowserRouter>
+            </HashRouter>
           </main>
         </div>
       </TooltipProvider>
